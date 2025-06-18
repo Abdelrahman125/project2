d22db91a9ab9f40d9553ee5aad31063df6f2b52b
Abdelrahman <abdelrahmanemad988@gmail.com>
2025-06-18 17:34:44 +0300

    the code for the project

---
+++ main.py
@@ -0,0 +1,18 @@
+print("🤖 Hello! I am your simple chatbot. Type 'bye' to exit.\n")
+
+responses = {
+    "hello": "Hi there!",
+    "how are you": "I'm just a program, but I'm doing fine!",
+    "what is your name": "I'm PyBot, your friendly assistant.",
+    "bye": "Goodbye! Have a nice day!"
+}
+
+while True:
+    user_input = input("You: ").lower()
+
+    if user_input == "bye":
+        print("Bot:", responses["bye"])
+        break
+
+    response = responses.get(user_input, "I don't understand that. Try something else.")
+    print("Bot:", response)
